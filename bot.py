8db0df14be5eb4e0db46e8d4c2ababd956f2d222
balconyRewrap <110861634+balconyRewrap@users.noreply.github.com>
2024-05-30 17:15:10 +0200

    Update bot.py
    Completely redesigned the work of the bot. Now the bot is implemented on a finite state machine, not on a list.

--- bot.py
+++ bot.py
@@ -1,160 +1,133 @@
 import telebot
-from config import TG_TOKEN, BOT_PASSWORD
 from telebot import types
+from config import TG_TOKEN, TG_ADMINS_ID
 from MedicalSurvey import MedicalSurvey
-from QuestionsHandlers import Questions, QuestionsNumber, question_handlers
+from QuestionsHandlers import get_first_question, get_next_question_by_id, Question, QuestionType, save_survey_date, handle_answer, get_question_by_id, get_next_question_id_by_id, get_last_question_by_id
 from datetime import datetime
-from DatabaseManager import add_medical_survey, get_medical_survey
-from typing import List
+from DatabaseManager import add_medical_survey
 bot = telebot.TeleBot(TG_TOKEN)
+survey = MedicalSurvey("")
 
+user_answers = {"user_id": 0, "answer_id": -1, "answer": ""}
 
-def ask_question(message: telebot.types.Message, survey: MedicalSurvey, question: str):
-    if question == Questions.Q1.value[0][0]:
-        current_date = datetime.now()
-        bot.send_message(message.chat.id, Questions.Q1.value[0][0])
-        formatted_date = current_date.strftime("%d-%m-%Y")
-        bot.send_message(message.chat.id, f"{formatted_date}")
-        msg = bot.send_message(message.chat.id, "Все верно?", reply_markup=get_keyboard(Questions.Q1.value[0][0]))
-        bot.register_next_step_handler(msg, lambda m: save_answer(survey, m, question))
-    elif question == Questions.Q5.value[0][0]:
-        msg = bot.send_message(message.chat.id, question, reply_markup=get_keyboard(Questions.Q1.value[0][0]))
-        bot.send_message(message.chat.id, f"{survey.age}")
-        bot.register_next_step_handler(msg, lambda m: save_answer(survey, m, question))
-    else:
-        msg = bot.send_message(message.chat.id, question, reply_markup=get_keyboard(question))
-        bot.register_next_step_handler(msg, lambda m: save_answer(survey, m, question))
-
-
-def handle_question(q: QuestionsNumber, survey: MedicalSurvey, message: telebot.types.Message):
-    question, number = question_handlers[q](survey, message)
-    if question == "end":
-        finish_registration(survey, message)
-    elif question == "Неправильная Дата Анкетирования":
-        bot.send_message(message.chat.id, "Неправильно введена дата анкетирования")
-        message.text = ""
-        ask_question(message, survey, Questions.Q1_1.value[0][0])
-    elif question == "Неправильная Дата Рождения":
-        bot.send_message(message.chat.id, "Неправильно введена Дата Рождения")
-        message.text = ""
-        ask_question(message, survey, Questions.Q4.value[0][0])
-    elif question == "Неправильное число" or question == "Неверный выбор" or question == "Неправильный пол":
-        bot.send_message(message.chat.id, question)
-        bot.send_message(message.chat.id, "Введите заново, пожалуйста:")
-        message.text = ""
-        attr_name = f"Q{number}"
-        question = getattr(Questions, attr_name).value[0][0]
-        ask_question(message, survey, question)
-    elif question == "назад":
-        bot.send_message(message.chat.id, "Вы вернулись на предыдущий шаг.")
-        if number == 1:
-            attr_name = f"Q{number}"
-        else:
-            attr_name = f"Q{number-1}"
-        question = getattr(Questions, attr_name).value[0][0]
-        ask_question(message, survey, question)
-    elif question == "start":
-        start(message)
-    elif question == "get":
-        get_survey(message)
-    else:
-        ask_question(message, survey, question)
-
-
-def save_answer(survey: MedicalSurvey, message: telebot.types.Message, question: str) -> None:
-    for q in QuestionsNumber:
-        if question.startswith(q.value):
-            handle_question(q, survey, message)
-            break
-
-
-def get_keyboard(question: str) -> types.ReplyKeyboardMarkup:
+def get_keyboard(question: Question) -> types.ReplyKeyboardMarkup:
     keyboard = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
-    for member in Questions.__members__.values():
-        if member.value[0][0] == question:
-            if member.value[0][1] is not None:
-                count = 0
-                for _ in member.value[0]:
-                    count += 1
-                if count == 3 or count == 6:
-                    for i in range(1, count):
-                        keyboard.add(types.KeyboardButton(text=member.value[0][i]))
-
-    else:
-        keyboard.add(types.InlineKeyboardButton(text="назад", callback_data="back"))
+    if question.has_options:
+        for option in question.options:
+            keyboard.add(types.KeyboardButton(text=option))
+    keyboard.add(types.KeyboardButton(text="Назад"))
     return keyboard
 
+# Функция для сохранения ответа пользователя
+def save_answer(current_question_id: int, message) -> bool:
+    global survey
+    if current_question_id == 0:
+        save_survey_date(survey)
+    try:
+        handle_answer(question_id=current_question_id,  survey=survey, message=message)
+        pass
+    except ValueError as e:
+        question= get_question_by_id(current_question_id)
+        if question:
+            if question.question_type == QuestionType.DATE.value:
+                bot.send_message(message.chat.id, "Неправильный формат даты. Пожалуйста, введите дату в формате ДД-ММ-ГГГГ")
+            elif question.question_type == QuestionType.CHOICE.value:
+                bot.send_message(message.chat.id, "Этого варианта нету в списке. Пожалуйста, выберите один из предложенных вариантов")
+            elif current_question_id == 41:
+                bot.send_message(message.chat.id, "Неправильный формат числа. Пожалуйста, введите число")
+        return True
+    return False
+
+def change_user_state(next_question_id: int, message):
+    user_answers["answer_id"] = next_question_id
+    user_answers["answer"] = message.text
+
+
+
+# Функция для отправки вопроса пользователю
+def send_question(chat_id, question):
+    formatted_date = ""
+    if question.id == 1:
+        current_date = datetime.now()
+        formatted_date = " " + current_date.strftime("%d-%m-%Y")
+    bot.send_message(chat_id, question.question_text + formatted_date, reply_markup=get_keyboard(question))
+
+@bot.message_handler(commands=['start']) 
+def start(message: telebot.types.Message) -> None:
+    global user_answers 
+    user_answers = {"user_id": message.chat.id, "answer_id": 0, "answer": ""}
+    send_question(message.chat.id, get_first_question())
+
+@bot.message_handler(func=lambda message: message.text == "Назад")
+def back(message: telebot.types.Message) -> None:
+    global user_answers 
+    answer_id = user_answers["answer_id"]
+    last_question = get_last_question_by_id(answer_id)
+    if last_question:
+        change_user_state(last_question.id, message)
+        send_question(message.chat.id, last_question)
 
 def finish_registration(survey: MedicalSurvey, message: telebot.types.Message) -> None:
     # Вывод всех данных
     survey.print_fields()
     add_medical_survey(survey)
     bot.send_message(message.chat.id, "Ваши данные успешно сохранены:\n")
+    for id in TG_ADMINS_ID:
+        bot.send_message(id, "Новая анкета заполнена:\n")
+        (formatted_data_main, formatted_data_conditions, formatted_data_health_events, formatted_data_family_history,
+        formatted_data_symptoms, formatted_data_lifestyle,
+        formatted_data_additional_complaints) = survey.format_data_as_html()
+        
+        bot.send_message(id, formatted_data_main, parse_mode='HTML')
+        bot.send_message(id, formatted_data_conditions, parse_mode='HTML')
+        bot.send_message(id, formatted_data_health_events, parse_mode='HTML')
+        bot.send_message(id, formatted_data_family_history, parse_mode='HTML')
+        bot.send_message(id, formatted_data_symptoms, parse_mode='HTML')
+        bot.send_message(id, formatted_data_lifestyle, parse_mode='HTML')
+        bot.send_message(id, formatted_data_additional_complaints, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['start'])
-def start(message: telebot.types.Message) -> None:
-    bot.send_message(message.chat.id, "Пожалуйста, введите номер вашего полиса:", reply_markup=types.ReplyKeyboardRemove())
-    bot.register_next_step_handler(message, process_policy_number)
-
-
-def process_policy_number(message: telebot.types.Message) -> None:
-    if message.text is None:
-        return
-    if message.text.startswith("/get"):
-        get_survey(message)
-        return
-    policy_number: str = message.text
-    survey: MedicalSurvey = MedicalSurvey(policy_number)
-    bot.send_message(message.chat.id, f"Спасибо! Ваш номер полиса: {policy_number}")
-    bot.send_message(message.chat.id, "Привет! Для заполнения анкеты, пожалуйста, ответьте на следующие вопросы:")
-    ask_question(message, survey, f"1. Дата анкетирования (день-месяц-год):")
-
-
-@bot.message_handler(commands=['get'])
-def get_survey(message: telebot.types.Message) -> None:
-    if message.text is None:
-        bot.send_message(message.chat.id, "Неправильный формат команды. Пожалуйста, "
-                                          "введите команду в формате /get пароль номер_полиса")
-        return
-    
-    command: List[str] = message.text.split()
-    if len(command) != 3:
-        bot.send_message(message.chat.id, "Неправильный формат команды. Пожалуйста, "
-                                          "введите команду в формате /get пароль номер_полиса")
-        return
-    
-    password: str = command[1]
-    policy_number: str = command[2]
-    
-    # Проверка пароля
-    if password != BOT_PASSWORD:
-        bot.send_message(message.chat.id, "Неправильный пароль")
-        return
-    
-    survey: MedicalSurvey | None = get_medical_survey(policy_number)
-    if survey is None:
-        bot.send_message(message.chat.id, "Анкета не найдена")
-        return
-    
-    (formatted_data_main, formatted_data_conditions, formatted_data_health_events, formatted_data_family_history,
-     formatted_data_symptoms, formatted_data_lifestyle,
-     formatted_data_additional_complaints) = survey.format_data_as_html()
-    
-    bot.send_message(message.chat.id, formatted_data_main, parse_mode='HTML')
-    bot.send_message(message.chat.id, formatted_data_conditions, parse_mode='HTML')
-    bot.send_message(message.chat.id, formatted_data_health_events, parse_mode='HTML')
-    bot.send_message(message.chat.id, formatted_data_family_history, parse_mode='HTML')
-    bot.send_message(message.chat.id, formatted_data_symptoms, parse_mode='HTML')
-    bot.send_message(message.chat.id, formatted_data_lifestyle, parse_mode='HTML')
-    bot.send_message(message.chat.id, formatted_data_additional_complaints, parse_mode='HTML')
-
-
-
+# Обработчик ответов от пользователя
 @bot.message_handler(func=lambda message: True)
-def echo_all(message: telebot.types.Message) -> None:
-    bot.send_message(message.chat.id,
-                     "Простите, я вас не понимаю. Если вы хотите заполнить анкету, то пропишите команду /start")
+def handle_message(message):
+    global survey
+    global user_answers
+    chat_id = message.chat.id
+    answer_id = user_answers["answer_id"]
+    if answer_id in range(0, 50):
+        current_question_id = user_answers["answer_id"]
+        next_question = get_next_question_by_id(current_question_id, message.text)
+        next_question_id = get_next_question_id_by_id(answer = message.text, id = current_question_id)
+        if next_question and next_question_id:
+            has_error = save_answer(current_question_id, message)
+            if has_error:
+                current_question = get_question_by_id(current_question_id)
+                change_user_state(current_question_id, message)
+                send_question(chat_id, current_question)
+            else:
+                change_user_state(next_question_id, message)
+                send_question(chat_id, next_question)
+        else:
+            has_error = save_answer(current_question_id, message)
+            if has_error:
+                current_question = get_question_by_id(current_question_id)
+                change_user_state(current_question_id, message)
+                send_question(chat_id, current_question)
+            else:
+                change_user_state(-1, message)
+            bot.send_message(chat_id, "Спасибо за заполнение анкеты!")
+            finish_registration(survey, message)
+    else:
+        bot.send_message(chat_id, "Чтобы начать анкету, введите /start")
+
+bot.polling()
+
+
+
+
+    
+
+
 
 
 bot.polling()
